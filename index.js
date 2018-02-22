22404b37ea9e03c9c8ae5222250e7b92575f171d
Yuhi Matsumoto <ym.starbuckslover@icloud.com>
2018-02-22 09:07:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string){
+  return string.toUpperCase()
+}
+
+function whisper(string){
+  return string.toLowerCase()
+}
+
+function logShout(string){
+  console.log('HELLO')
+}
+
+function logWhisper(string){
+  console.log('hello')
+}
+
+function sayHiToGrandma(string){
+  sayHiToGrandma('hello').toEqual()
+  return string.toLowerCase()
+}
+
+function sayHiToGrandma(string){
+
+}
+
+function sayHiToGrandma(string){
+
+}
